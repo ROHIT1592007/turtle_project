7f016e83dfddc420cd79deb02a520fe92286f1af
K.V.ROHIT <87985502+ROHIT1592007@users.noreply.github.com>
2021-11-30 19:55:50 +0530

    Add files via upload

---
+++ hexagon.py
@@ -0,0 +1,9 @@
+import turtle
+
+hexagon = turtle.Turtle
+
+for i in range (6):
+    turtle.forward(100)
+    turtle.left(60)
+
+turtle.done()
---
+++ spiral.py
@@ -0,0 +1,9 @@
+import turtle
+
+cir = turtle.Turtle()
+
+for i in range (10):
+    cir.circle(10*i)
+
+
+turtle.done()
